2cdb689d2e4d6cdf04d1b3cc09abcd4e06a3514a
Kate <katebancroft@yahoo.co.uk>
2025-04-03 10:20:55 +0100

    Random hex generator - my attempt without tutorial

--- 01-color-flipper/my_pract/hex.js
+++ 01-color-flipper/my_pract/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+    // // generate random hex
+    const randomHex = ["#"];
+    for(let i = 0; i <= 5; i++){
+        randomHex.push(hex[getRandomNumber()]);
+    };
+    
+    console.log(randomHex);
+    document.body.style.backgroundColor = randomHex.join("");
+    color.textContent = randomHex.join("");
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length); // returns random number, from 0 to 15
+};
