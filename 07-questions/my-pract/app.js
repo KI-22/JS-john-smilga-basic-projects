1498f2ccbca79b0e5f1466fc07eec9c781110e6e
Kate <katebancroft@yahoo.co.uk>
2025-04-04 17:09:22 +0100

    Mostly works, only missing 'only 1 question at a time showing' functionality

--- 07-questions/my-pract/app.js
+++ 07-questions/my-pract/app.js
@@ -1,2 +1,14 @@
-//using selectors inside the element
-// traversing the dom
+// opt 2 - using selectors inside the element
+// opt 1 - traversing the dom (currently attempting below)
+
+
+// select buttons
+const btns = document.querySelectorAll('.question-btn');    // using All b.c. we have mutiple buttons
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(el) {
+        console.log(el.currentTarget.parentElement.parentElement);
+        const question = el.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");  // toggle seems to add/remove as relevant
+    });
+});
