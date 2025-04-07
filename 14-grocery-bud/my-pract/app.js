087d0d5eb788a081c0d1c3d96bbfa06d1664919d
Kate <katebancroft@yahoo.co.uk>
2025-04-07 16:26:49 +0100

    Complete! All working now, including local storage

--- 14-grocery-bud/my-pract/app.js
+++ 14-grocery-bud/my-pract/app.js
@@ -22,6 +22,8 @@ let editID = "";
 form.addEventListener("submit", addItem);
 // clear items
 clearBtn.addEventListener("click", clearItems);
+// load items
+window.addEventListener("DOMContentLoaded", setupItems);
 
 
 
@@ -36,30 +38,32 @@ function addItem(e){
     // console.log(id);
 
     if (value && !editFlag){
-        // console.log("add item to the list");
-        const element = document.createElement("article");
-        // add class
-        element.classList.add("grocery-item");
-        // add id 
-        const attr = document.createAttribute("data-id");           // didn't fully understand below here but works!
-        attr.value = id;
-        element.setAttributeNode(attr);
-        element.innerHTML = `<p class="title">${value}</p>  
-              <div calss="btn-container">
-                <button type="button" class="edit-btn">
-                  <i class="fas fa-edit"></i>
-                </button>
-                <button type="button" class="delete-btn">
-                  <i class="fas fa-trash"></i>
-                </button>
-              </div>`;
-        const deleteBtn = element.querySelector(".delete-btn");
-        const editBtn = element.querySelector(".edit-btn");
-        deleteBtn.addEventListener("click", deleteItem);
-        editBtn.addEventListener("click", editItem);
+        // // console.log("add item to the list");
+        // const element = document.createElement("article");
+        // // add class
+        // element.classList.add("grocery-item");
+        // // add id 
+        // const attr = document.createAttribute("data-id");           // didn't fully understand below here but works!
+        // attr.value = id;
+        // element.setAttributeNode(attr);
+        // element.innerHTML = `<p class="title">${value}</p>  
+        //       <div calss="btn-container">
+        //         <button type="button" class="edit-btn">
+        //           <i class="fas fa-edit"></i>
+        //         </button>
+        //         <button type="button" class="delete-btn">
+        //           <i class="fas fa-trash"></i>
+        //         </button>
+        //       </div>`;
+        // const deleteBtn = element.querySelector(".delete-btn");
+        // const editBtn = element.querySelector(".edit-btn");
+        // deleteBtn.addEventListener("click", deleteItem);
+        // editBtn.addEventListener("click", editItem);
+        // // append child
+        // list.appendChild(element);
 
-        // append child
-        list.appendChild(element);
+        createListItem(id, value);
+
         // display alert
         displayAlert("item added", "success");
         // show container (our list items)
@@ -74,7 +78,7 @@ function addItem(e){
         editElement.innerHTML = value;
         displayAlert("value changed", "success");
         // edit local storage
-        // editLocalStorage(editID, value);
+        editLocalStorage(editID, value);
         setBackToDefault();
     }
     else{
@@ -122,10 +126,11 @@ function deleteItem(e) {
     displayAlert("item removed", "danger");
     setBackToDefault();
     // remove from local storage
-    // removeFromLocalStorage(id);
+    removeFromLocalStorage(id);
 };
 
-// set back to default (most are extras for now, will need later!)
+
+// set back to default
 function setBackToDefault(){
     // console.log("set back to default");
     grocery.value = "";
@@ -143,25 +148,104 @@ function clearItems(){
             list.removeChild(item);
         });
         container.classList.remove("show-container"); // no items to show but helps hide clear-btn
-        displayAlert("items deleted", "danger");
+        displayAlert("empty list", "danger");
         setBackToDefault();
-        // localStorage.removeItem("list");
+        localStorage.removeItem("list");
     }
 };
 
 
 
+
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value){
-    console.log("add to local storage:" + value);
+    const grocery = {id, value};
+    // let items = localStorage.getItem("list")        // this part checks whether item exists or not...
+    //     ? JSON.parse(localStorage.getItem("list"))  // ...if it does, then get me that item (using Json.parse)
+    //     : [];                                       // ...if it doesn't, defaults to empty array (i think)
+    let items = getLocalStorage();
+    items.push(grocery);
+    localStorage.setItem("list", JSON.stringify(items));
+    console.log(items);
 };
+
 
 function removeFromLocalStorage(id){
-    //
+    let items = getLocalStorage();
+    
+    items = items.filter(function(item){    // filter out those that don't match id
+        if (item.id != id) {  
+            return item;
+        }
+    });
+    localStorage.setItem("list", JSON.stringify(items));
 };
+
 
 function editLocalStorage(id, value){
-    //
+    let items = getLocalStorage();
+    items = items.map(function(item) {
+        if (item.id === id) {  
+            item.value = value;
+        }
+        return item;
+    });
+    localStorage.setItem("list", JSON.stringify(items));
 };
 
+
+function getLocalStorage(){
+    return localStorage.getItem("list")        // this part checks whether item exists or not...
+        ? JSON.parse(localStorage.getItem("list"))  // ...if it does, then get me that item (using Json.parse)
+        : [];   
+}
+
+
+// localStorageAPI << can see on Chrome Developer window under 'Application'
+// setItem
+// getItem
+// save as strings
+
+// localStorage.setItem('orange', JSON.stringify(['item', 'item2']));
+// const oranges = JSON.parse(localStorage.getItem('orange'));
+// console.log(oranges);
+// localStorage.removeItem('orange');
+
+
 // ****** SETUP ITEMS **********
+function setupItems(){
+    let items = getLocalStorage();
+    if (items.length > 0){
+        items.forEach(function(item){
+            createListItem(item.id, item.value); // as on Applicatoin is id:..., value:...
+        })
+    container.classList.add("show-container");
+    }
+};
+
+function createListItem(id, value){
+    // console.log("add item to the list");
+    const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id 
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>  
+            <div calss="btn-container">
+            <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+            </button>
+            <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+            </button>
+            </div>`;
+    const deleteBtn = element.querySelector(".delete-btn");
+    const editBtn = element.querySelector(".edit-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+
+    // append child
+    list.appendChild(element);
+}
